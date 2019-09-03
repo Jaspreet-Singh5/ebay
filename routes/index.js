75be8e4259b5ee0894e610e94c4b16073bf37f54
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2019-09-03 21:37:06 +0530

    Updated files

--- routes/index.js
+++ routes/index.js
@@ -23,38 +23,15 @@ db.connect((err) => {
 router.get("/", function (req, res) {
 
     let sql1 = `
-        SELECT DISTINCT title, image, description, price
-        FROM courses 
-        INNER JOIN best_courses ON courses.id=best_courses.COURSE_ID;
+        SELECT DISTINCT course_name, course_description
+        FROM courses;
     `;
+    
+    let query1 = db.query(sql1, (err, Courses) => {
+         if (err) throw err;
+         console.log(Courses);
 
-    let sql2 = `
-        SELECT DISTINCT title, image, description, price
-        FROM courses 
-        INNER JOIN latest_courses ON courses.id=latest_courses.COURSE_ID;
-    `;
-
-    let sql3 = `
-        SELECT DISTINCT title, image, description, price
-        FROM courses 
-        INNER JOIN onsale_courses ON courses.id=onsale_courses.COURSE_ID;
-    `;
-
-    let query1 = db.query(sql1, (err, bestCourses) => {
-        if (err) throw err;
-        console.log(bestCourses);
-
-        let query2 = db.query(sql2, (err, latestCourses) => {
-            if (err) throw err;
-            console.log(latestCourses);
-
-            let query3 = db.query(sql3, (err, onsaleCourses) => {
-                if (err) throw err;
-                console.log(onsaleCourses);
-
-                res.render("landing.ejs", { bestCourses: bestCourses, latestCourses: latestCourses, saleCourses: onsaleCourses });
-            });
-        });
+         res.render("landing.ejs", { Courses: Courses });  
      });
 });
 
